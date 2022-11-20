6900785f9502ecea900af10f9ef7a0eeb03c7026
Shrinivas Biradar <shrinivas.biradar@techdome.net.in>
2022-11-20 21:25:20 +0530

    initial commit

---
+++ apps/portfolio/pages/index.js
@@ -0,0 +1,23 @@
+import styles from './index.module.scss';
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.scss file.
+   */
+  return (
+    <div className={styles.page}>
+      <div className="wrapper">
+        <div className="container">
+          <div id="welcome">
+            <h1>
+              <span> Hello there, </span>
+              Welcome portfolio 👋
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Index;
